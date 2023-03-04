4b14dd55fe32428f99202bbaba620b7af42a7059
huypa <96anhhuyndu1415@gmail.com>
2023-03-04 18:24:24 +0700

    main

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,8 +1,27 @@
+WITH fact_sales_order_line_source AS (
+  SELECT 
+    order_line_id 
+  , stock_item_id 
+  , quantity 
+  , unit_price 
+  FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+)
+
+, fact_sales_order_line_rename as (
+  SELECT 
+    cast(order_line_id as int) as sales_order_line_key
+  , cast(stock_item_id as int) as product_key
+  , cast(quantity as int) as quantity
+  , cast(unit_price as numeric ) as unit_price
+  --, cast((quantity * unit_price) as numeric ) as gross_amount
+  , cast(quantity as int) * cast(unit_price as numeric) as gross_amount
+  FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+)
+
 SELECT 
-  cast(order_line_id as int) as sales_order_line_key
-, cast(stock_item_id as int) as product_key
-, cast(quantity as int) as quantity
-, cast(unit_price as numeric ) as unit_price
---, cast((quantity * unit_price) as numeric ) as gross_amount
-, cast(quantity as int) * cast(unit_price as numeric) as gross_amount
-FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+  sales_order_line_key
+, product_key
+, quantity
+, unit_price
+, gross_amount
+FROM fact_sales_order_line_rename
