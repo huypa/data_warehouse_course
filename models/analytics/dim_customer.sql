74e1173804bbc2a11aa4fbf785e1611a8d7130a6
huypa <96anhhuyndu1415@gmail.com>
2023-03-04 18:35:57 +0700

    rename

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,17 @@
-SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.sales__customers`
+with customer_info_source as (
+select  
+  customer_id
+, customer_name
+from `vit-lam-data.wide_world_importers.sales__customers`
+group by 1,2
+)
+, customer_info_rename as (
+select 
+  customer_id as customer_key
+, customer_name as customer_name
+from customer_info_source
+)
+select 
+  customer_key
+, customer_name
+from customer_info_rename
