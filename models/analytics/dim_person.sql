4f8483328728c4c0950197a82d54c8ed478d67ba
huypa <96anhhuyndu1415@gmail.com>
2023-03-11 18:01:26 +0700

    #

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -1,3 +1,16 @@
+with dim_person_source as (
 SELECT 
   *
 FROM `vit-lam-data.wide_world_importers.application__people`
+)
+, dim_person_rename_cast as (
+SELECT 
+    cast(person_id  as int ) as person_key
+  , cast(full_name as string ) as full_name
+FROM dim_person_source
+)
+SELECT 
+    person_key
+  , full_name
+FROM dim_person_rename_cast
+GROUP BY 1,2
