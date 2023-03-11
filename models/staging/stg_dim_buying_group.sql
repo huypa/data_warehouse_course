0b5cda571df1cb8f8ea56ba3b3a97581904df26f
huypa <96anhhuyndu1415@gmail.com>
2023-03-11 16:43:14 +0700

    #

--- models/staging/stg_dim_buying_group.sql
+++ models/staging/stg_dim_buying_group.sql
@@ -4,12 +4,12 @@ with dim_buying_group_source as (
 )
 , dim_buying_group_rename_cast as (
   select
-      cast(buying_group_id as int) as buying_group_key
+    cast(buying_group_id as int) as buying_group_key
     , cast(buying_group_name as string) as buying_group_name
   from dim_buying_group_source
 )
 select 
     buying_group_key
-  , buying_group_name
+    , buying_group_name
 from dim_buying_group_rename_cast
 group by 1,2
