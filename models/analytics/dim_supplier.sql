2c95f771630a6af73c99eac014ee09d3e30d6081
huypa <96anhhuyndu1415@gmail.com>
2023-03-11 13:02:42 +0700

    main

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,3 +1,16 @@
+with dim_supplier_source as (
 SELECT 
   *
 FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+)
+, dim_supplier_rename_cast as (
+SELECT 
+    cast(supplier_id  as int ) as supplier_key
+  , cast(supplier_name as string ) as supplier_name
+FROM dim_supplier_source
+)
+SELECT 
+    supplier_key
+  , supplier_name
+FROM dim_supplier_rename_cast
+GROUP BY 1,2
