db87d97ff63042bb4bb91fccc3682e1185438dbf
huypa <96anhhuyndu1415@gmail.com>
2023-03-11 16:51:13 +0700

    #

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -5,15 +5,17 @@ with dim_product_source as (
 , dim_product_rename_cast as (
 SELECT  
   cast(stock_item_id as int ) as product_key
-, cast(stock_item_name as string ) as  product_name
-, cast(brand as string ) as  brand_name 
-, cast(supplier_id as int) as supplier_key
+  , cast(stock_item_name as string ) as  product_name
+  , cast(brand as string ) as  brand_name 
+  , cast(supplier_id as int) as supplier_key
+  , cast(is_chiller_stock as boolean) as is_chiller_stock
 FROM dim_product_source
 )
 select 
-    dim_product.product_key
+  dim_product.product_key
   , dim_product.product_name
   , dim_product.brand_name
+  , dim_product.is_chiller_stock
   , dim_product.supplier_key
   , dim_supplier.supplier_name
 from dim_product_rename_cast as dim_product
