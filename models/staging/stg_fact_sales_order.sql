00c414b3bf76384ba129a5a8793e33a2e11c6c73
huypa <96anhhuyndu1415@gmail.com>
2023-03-11 18:05:32 +0700

    #

--- models/staging/stg_fact_sales_order.sql
+++ models/staging/stg_fact_sales_order.sql
@@ -4,12 +4,14 @@ with fact_sales_order_source as (
 )
 , fact_sales_order_cast_type as (
 select 
-    cast(order_id as int) as sales_order_key
+  cast(order_id as int) as sales_order_key
   , cast(customer_id as int) as customer_key
+  , cast(picked_by_person_id as int) as picked_by_person_key
 from fact_sales_order_source
 )
 select 
   sales_order_key
-, customer_key
+  , customer_key
+  , picked_by_person_key
 from fact_sales_order_cast_type
 group by 1,2
