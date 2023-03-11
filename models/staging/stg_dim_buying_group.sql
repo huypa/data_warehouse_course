caf34ee61293612cd59bd89e9815e20d9854687c
huypa <96anhhuyndu1415@gmail.com>
2023-03-11 16:14:11 +0700

    main

---
+++ models/staging/stg_dim_buying_group.sql
@@ -0,0 +1,15 @@
+with dim_buying_group_source as (
+  select * 
+  from `vit-lam-data.wide_world_importers.sales__buying_groups`
+)
+, dim_buying_group_rename_cast as (
+  select
+      cast(buying_group_id as int) as buying_group_key
+    , cast(buying_group_name as string) as buying_group_name
+  from dim_buying_group_source
+)
+select 
+    buying_group_key
+  , buying_group_name
+from dim_buying_group_rename_cast
+group by 1,2
---
+++ models/staging/stg_dim_customer_category.sql
@@ -0,0 +1,15 @@
+with dim_customer_category_source as (
+  select * 
+  from `vit-lam-data.wide_world_importers.sales__customer_categories`
+)
+, dim_customer_category_rename_cast as (
+  select
+      cast(customer_category_id as int) as customer_category_key
+    , cast(customer_category_name as string) as customer_category_name
+  from dim_customer_category_source
+)
+select 
+    customer_category_key
+  , customer_category_name
+from dim_customer_category_rename_cast
+group by 1,2
