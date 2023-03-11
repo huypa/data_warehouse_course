39ca59324a56c8841f8dd9af07f829e524fcaa74
huypa <96anhhuyndu1415@gmail.com>
2023-03-11 11:56:52 +0700

    main

--- models/staging/stg_fact_sales_order_line.sql
+++ models/staging/stg_fact_sales_order_line.sql
@@ -1,10 +1,15 @@
-with fact_sales_order_cast_type as (
+with fact_sales_order_source as (
+  select *
+  from `vit-lam-data.wide_world_importers.sales__orders`
+)
+, fact_sales_order_cast_type as (
 select 
     cast(order_id as int) as sales_order_key
   , cast(customer_id as int) as customer_key
-from `vit-lam-data.wide_world_importers.sales__orders`
+from fact_sales_order_source
 )
 select 
   sales_order_key
 , customer_key
 from fact_sales_order_cast_type
+group by 1,2
