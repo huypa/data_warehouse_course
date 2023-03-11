791fae6ac5bd54c145a32325ac07acd6b091375d
huypa <96anhhuyndu1415@gmail.com>
2023-03-11 17:01:07 +0700

    #

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -11,6 +11,19 @@ SELECT
   , cast(is_chiller_stock as boolean) as is_chiller_stock
 FROM dim_product_source
 )
+, dim_product_final as (
+  SELECT  
+  product_key
+  , product_name
+  , brand_name 
+  , supplier_key
+  , is_chiller_stock
+  , case when is_chiller_stock is true then "Chiller_stock"
+         when is_chiller_stock is false then "Non chillder stock"
+         else "Unknown"
+    end as is_chiller_stock
+FROM dim_product_source
+)
 select 
   dim_product.product_key
   , dim_product.product_name
@@ -18,6 +31,6 @@ select
   , dim_product.is_chiller_stock
   , dim_product.supplier_key
   , dim_supplier.supplier_name
-from dim_product_rename_cast as dim_product
+from dim_product_final as dim_product
 left join {{ref('dim_supplier')}} as dim_supplier 
   on dim_product.supplier_key = dim_supplier.supplier_key
