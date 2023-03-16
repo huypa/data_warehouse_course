a5ee1f22ca5d5f8f519ce1637a75d1bf18ccc129
huypa <96anhhuyndu1415@gmail.com>
2023-03-16 19:24:41 +0700

    #

---
+++ models/analytics/dim_date.sql
@@ -0,0 +1,26 @@
+with dim_date_generate as (
+SELECT
+  d AS date,
+  FORMAT_DATE('%A', d) AS day_of_week,
+  FORMAT_DATE('%a', d) AS day_of_week_short,
+  
+  date_trunc(d,month) as year_month,
+  EXTRACT(month FROM d) AS month,
+  date_trunc(d,year) as year,
+  EXTRACT(YEAR FROM d) AS year_number
+FROM (
+  SELECT
+    *
+  FROM
+    UNNEST(GENERATE_DATE_ARRAY('2014-01-01', '2050-01-01', INTERVAL 1 DAY)) AS d )
+)
+, dim_date_generate_enrich as (
+  SELECT 
+  *
+  , CASE WHEN day_of_week IN ('Sunday', 'Saturday') THEN "Weekend" 
+         WHEN day_of_week IN ('Monday','Tuesday','Wednesday','Thursday','Friday') THEN "Weekday"
+    ELSE 'Invalid' END AS is_week_day_or_weekend
+  FROM dim_date_generate
+)
+SELECT * 
+FROM dim_date_generate_enrich
