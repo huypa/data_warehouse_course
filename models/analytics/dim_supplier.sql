1583a1cddb45bd5f08097d67e9275e44c9b7ce29
huypa <96anhhuyndu1415@gmail.com>
2023-03-19 16:46:45 +0700

    #

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -7,10 +7,45 @@ FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
 SELECT 
     cast(supplier_id  as int ) as supplier_key
   , cast(supplier_name as string ) as supplier_name
+  , cast(Primary_Contact_Person_ID as int ) as primary_contact_person_key
+  , cast(Alternate_Contact_Person_ID as int ) as alternate_contact_person_key
+  , cast(Delivery_Method_ID as int ) as delivery_method_key
+  , cast(Delivery_City_ID as int ) as delivery_city_key
+  , cast(Bank_account_name as string ) as Bank_account_name
+  , cast(Bank_account_branch as string ) as Bank_account_branch
+  , cast(Bank_account_code as string ) as Bank_account_code
+  , cast(Bank_account_number as string ) as Bank_account_number
+  , cast(Bank_international_code as string ) as Bank_international_code
+  , cast(Phone_number as string ) as Phone_number
+  , cast(Fax_Number as string ) as Fax_Number
 FROM dim_supplier_source
 )
+select * from dim_supplier_source
 SELECT 
-    supplier_key
-  , supplier_name
-FROM dim_supplier_rename_cast
-GROUP BY 1,2
+    dim_supplier.Supplier_key
+  , dim_supplier.Supplier_name
+  , dim_supplier.Bank_account_name
+  , dim_supplier.Bank_account_branch
+  , dim_supplier.Bank_account_code
+  , dim_supplier.Bank_account_number
+  , dim_supplier.Bank_international_code
+  , dim_supplier.Phone_number
+  , dim_supplier.Fax_Number
+  , dim_supplier_category.Supplier_category_key
+  , dim_supplier_category.Supplier_category_name
+  , dim_delivery_method.Delivery_method_key
+  , dim_delivery_method.Delivery_method_name 
+  , dim_city.city_key as Delivery_city_key 
+  , dim_city.city_name as Delivery_city_name 
+  , dim_city.province_name as Delivery_province_name
+  , dim_city.countries_name as Delivery_countries_name
+  , dim_people.person_key as Primary_contact_person_key
+  , dim_people.full_name as Primary_contact_full_name
+  , dim_people.person_key as Alternate_contact_person_key
+  , dim_people.full_name as Alternate_contact_full_name
+FROM dim_supplier_rename_cast as dim_supplier
+left join {{ref('stg_dim_supplier_category')}} as dim_supplier_category on dim_supplier_category.delivery_method_key = dim_supplier.delivery_method_key
+left join {{ref('stg_dim_delivery_method')}} as dim_delivery_method on dim_delivery_method.delivery_method_key = dim_supplier.delivery_method_key
+left join {{ref('stg_dim_city')}} as dim_city on dim_city.city_key = dim_supplier.delivery_city_key
+left join {{ref('dim_person')}} as dim_people on dim_people.person_key = dim_supplier.primary_contact_person_key
+left join {{ref('dim_person')}} as dim_people on dim_people.person_key = dim_supplier.alternate_contact_person_key
