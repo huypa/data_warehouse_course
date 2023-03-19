3db71d34daab100cb272979b848a5b3d965436b6
huypa <96anhhuyndu1415@gmail.com>
2023-03-19 15:49:45 +0700

    z3

--- models/staging/stg_dim_state_province.sql
+++ models/staging/stg_dim_state_province.sql
@@ -1,22 +1,22 @@
-with dim__source as (
+with dim_province_source as (
   select * 
   from `vit-lam-data.wide_world_importers.application__state_provinces` 
 )
-, dim__rename_cast as (
+, dim_province_rename_cast as (
 SELECT DISTINCT
-  cast(state__id as int ) as _key
-  , cast(state__name as string ) as  _name
+  cast(state_province_id as int ) as _key
+  , cast(state_province_name as string ) as  _name
   , cast(country_id as int ) as  countries_key
-FROM dim__source
+FROM dim_province_source
 )
-, dim__final as (
+, dim_province_final as (
 SELECT  *
-FROM dim__rename_cast
+FROM dim_province_rename_cast
 )
 select 
-  dim_._key
-  , dim_._name
+  dim_province._key
+  , dim_province._name
   , dim_countries.countries_name
-from dim__final as dim_
+from dim_province_final as dim_province
 left join {{ref('stg_dim_countries')}} as dim_countries
-  on dim_.countries_key = dim_countries.countries_key
+  on dim_province.countries_key = dim_countries.countries_key
