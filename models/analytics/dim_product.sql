5add3a551e4165b0d3b08a60661561c1888bf2ef
huypa <96anhhuyndu1415@gmail.com>
2023-03-19 17:18:17 +0700

    #

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -7,8 +7,14 @@ SELECT
   cast(stock_item_id as int ) as product_key
   , cast(stock_item_name as string ) as  product_name
   , cast(brand as string ) as  brand_name 
+  , cast(size as string ) as  size 
   , cast(supplier_id as int) as supplier_key
   , cast(is_chiller_stock as boolean) as is_chiller_stock
+  , cast(unit_price as float64) as unit_price
+  , cast(quantity_per_outer as int) as quantity_per_outer
+  , cast(typical_weight_per_unit as float64) as typical_weight_per_unit
+  , cast(barcode as string) as bar_code
+  , cast(lead_time_days as int) as lead_time_days  
 FROM dim_product_source
 )
 , dim_product_final as (
@@ -28,8 +34,13 @@ select
   dim_product.Product_key
   , dim_product.Product_name
   , coalesce(dim_product.brand_name,"Undefined") as Brand_name
+  , dim_product.Size
   , dim_product.Is_chiller_stock
-  , dim_product.Supplier_key
+  , dim_product.Unit_price
+  , dim_product.Quantity_per_outer
+  , dim_product.Typical_weight_per_unit
+  , dim_product.Bar_code
+  , dim_product.Lead_time_days
   , coalesce(dim_supplier.supplier_name,"Invalid") as Supplier_name
   , dim_supplier_category.Supplier_category_key
   , dim_supplier_category.Supplier_category_name
