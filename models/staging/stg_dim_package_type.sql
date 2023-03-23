7103379f8fc5e636107e74d6d7b6c86988472f68
huypa <96anhhuyndu1415@gmail.com>
2023-03-23 19:02:04 +0700

    #

--- models/staging/stg_dim_package_type.sql
+++ models/staging/stg_dim_package_type.sql
@@ -5,12 +5,12 @@ FROM `vit-lam-data.wide_world_importers.warehouse__package_types`
 )
 , dim_package_type_rename_cast as (
 SELECT 
-    cast(package_type_id  as int ) as package_type_key
+  cast(package_type_id  as int ) as package_type_key
   , cast(package_type_name as string ) as package_type_name
 FROM dim_package_type_source
 )
 SELECT 
-    package_type_key
+  package_type_key
   , package_type_name
 FROM dim_package_type_rename_cast
 GROUP BY 1,2
