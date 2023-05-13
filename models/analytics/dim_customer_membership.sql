9d8c60b2b7ad6cc05c62fcc9db9bea04fbe3e353
huypa <96anhhuyndu1415@gmail.com>
2023-05-13 16:27:42 +0700

    #

---
+++ models/analytics/dim_customer_membership.sql
@@ -0,0 +1,23 @@
+with dim_membership__source as (
+SELECT 
+  *
+FROM `vit-lam-data.wide_world_importers.external__customer_membership`
+)
+, dim_membership__rename_cast as (
+SELECT 
+    cast(customer_id  as int ) as customer_key
+  , cast(membership as string ) as membership
+  , cast(begin_effective_date as date ) as begin_effective_date
+  , cast(end_effective_date as date ) as end_effective_date
+FROM dim_membership__source
+)
+SELECT 
+  customer_key
+  , membership
+  , begin_effective_date
+  , end_effective_date
+FROM dim_membership__rename_cast
+
+
+		
+	
